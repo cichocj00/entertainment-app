0a1d03da7942d4a114e5b45f38fd01f51fdb10e0
cichocj00 <cichocjagh@gmail.com>
2024-04-19 14:05:04 +0200

    refactor navbar, add routes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Logo from '@/assets/logo.svg?react';
-import NavHome from '@/assets/icon-nav-home.svg?react';
-import NavBookmark from '@/assets/icon-nav-bookmark.svg?react';
-import NavMovies from '@/assets/icon-nav-movies.svg?react';
-import NavTvSeries from '@/assets/icon-nav-tv-series.svg?react';
 import avatar from '@/assets/image-avatar.png';
 import clsx from 'clsx';
+import { navbarItems } from '@/lib/constants';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = ({ styles }: { styles: string }): JSX.Element => {
+  const { pathname } = useLocation();
+
   return (
     <div
       className={clsx(
@@ -14,12 +14,23 @@ const Navbar = ({ styles }: { styles: string }): JSX.Element => {
         'flex w-full max-w-screen-md items-center justify-between rounded-xl bg-blue-semiDark px-4 py-[18px] lg:max-h-full lg:w-[96px] lg:flex-col lg:px-0 lg:py-12',
       )}
     >
-      <Logo />
-      <div className="flex gap-6 lg:mt-[-450px] lg:flex-col">
-        <NavHome className=" size-5 cursor-pointer fill-white hover:fill-red md:size-7" />
-        <NavMovies className="size-5 cursor-pointer hover:fill-red md:size-7" />
-        <NavTvSeries className="size-5 cursor-pointer hover:fill-red md:size-7" />
-        <NavBookmark className="size-5 cursor-pointer hover:fill-red md:size-7" />
+      <Link to="/">
+        <Logo />
+      </Link>
+      <div className="flex gap-6 lg:mt-[-350px] lg:flex-col">
+        {navbarItems.map((item) => {
+          const Icon = item.icon;
+          return (
+            <Link to={item.route} key={item.name}>
+              <Icon
+                className={clsx(
+                  pathname === item.route ? 'fill-white' : '',
+                  'size-5 cursor-pointer hover:fill-red md:size-7',
+                )}
+              />
+            </Link>
+          );
+        })}
       </div>
       <div className="size-7 md:size-10">
         <img src={avatar} alt="avatar" className="w-full" />
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,6 +1,34 @@
+import NavHome from '@/assets/icon-nav-home.svg?react';
+import NavBookmark from '@/assets/icon-nav-bookmark.svg?react';
+import NavMovies from '@/assets/icon-nav-movies.svg?react';
+import NavTvSeries from '@/assets/icon-nav-tv-series.svg?react';
+
 export const routes = {
   HOME: '/',
   MOVIES: '/movies',
   TVSERIES: '/tvseries',
   BOOKMARKED: '/bookmarked',
 };
+
+export const navbarItems = [
+  {
+    name: 'Home',
+    icon: NavHome,
+    route: routes.HOME,
+  },
+  {
+    name: 'Movies',
+    icon: NavMovies,
+    route: routes.MOVIES,
+  },
+  {
+    name: 'TV Series',
+    icon: NavTvSeries,
+    route: routes.TVSERIES,
+  },
+  {
+    name: 'Bookmarked',
+    icon: NavBookmark,
+    route: routes.BOOKMARKED,
+  },
+];
